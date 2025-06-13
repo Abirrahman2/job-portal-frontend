c904349c7182cd1fb33d9dc7d03c85181f13fbfe
MD MAHMUDUR RAHMAN <105537287+Abirrahman2@users.noreply.github.com>
2025-06-14 01:13:22 +0600

    good night

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
+'use client'
 import Image from "next/image";
+import BlurText from "./ui/Home/BlurText";
 
 export default function Home() {
+  const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
   return (
-    <>
-       hello world
-    </>
+    
+     <div className="min-h-screen pt-10">
+      <div className="max-w-5xl mx-auto ml-10">
+        <BlurText
+          text="WELCOME TO JOB PORTAL"
+          delay={150}
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleAnimationComplete}
+          className="text-5xl text-orange-500 mb-8 translate-x-130"
+        />
+      </div>
+     
+
+    </div>
+   
+    
+    
+    
+     
+    
+
+    
+     
+      
+  
   );
 }
